128a39e353712d5e06036e3cc45a04b5e48ddbf2
Mohammad Raafe <mraafe2010@gmail.com>
2025-04-27 00:43:10 +0400

    updated bloom_filter.c

--- bloom_filter.c
+++ bloom_filter.c
@@ -6,25 +6,7 @@ vaccination records management system.
 // importing relevant libraries
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
-// defining bloom filter structure
-typedef struct
-{
-   unsigned char *bits; // the bit array
-   unsigned int size;   // the size of the array
-} BloomFilter;
-
-/*
-function prototypes
-*/
-BloomFilter *bloom_create(unsigned int size);               // function to create a new bloom filter
-void bloom_delete(BloomFilter *filter);                     // function to delete an existing bloom filter
-void bloom_insert(BloomFilter *filter, const char *record); // function to insert record into filter
-bool bloom_check(BloomFilter *filter, const char *record);  // function to check if record exists in filter
-unsigned int hash_1(const char *str, unsigned int size);    // first hash function
-unsigned int hash_2(const char *str, unsigned int size);    // second hash function
-unsigned int hash_3(const char *str, unsigned int size);    // third hash function
+#include "bloom_filter.h"
 
 // implementing bloom_create(...) to create a new bloom filter structure
 // unsigned is used to ensure there are no negative values and that
